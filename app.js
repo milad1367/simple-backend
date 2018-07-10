99dae6fa1d27d21c5b8004462d283a282fd2386f
milad asghari <miladasghari2016@gmail.com>
2018-07-10 21:54:08 +0430

    add login

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+var port = 9000;
+app.listen(port);
+const cors = require('cors')
+app.use(cors())
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+console.log('The magic happens on port ' + port);
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api/authenticate',(req, res,next) => {
+    res.send({
+        cats: [{ name: 'lilly' }, { name: 'lucy' }]
+      });
+})
+app.post('/api/authenticate',(req, res,next) => {
+    if((req.body.username == "admin")&&(req.body.password == "1")) {
+        console.log(req.body)
+
+        res.send ({allow: true});
+    }
+    else {
+        res.send ({});
+    }    
+})
+//app.listen(3000, () => console.log('Example app listening on port 3000!'))
